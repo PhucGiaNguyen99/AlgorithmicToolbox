6573e926d84fdae793f269d596e8cbc1efe91fe2
Phuc Nguyen <66092633+Saotalangim161099@users.noreply.github.com>
2023-01-07 14:08:46 -0500

    Week 3- Greedy Algorithm Method maxLoot

--- src/W3GreedyAlgorithm.java
+++ src/W3GreedyAlgorithm.java
@@ -1,14 +1,25 @@
 public class W3GreedyAlgorithm {
-    public void printName(String name) {
-        System.out.println(name);
+    public static int maxLoot(int[] c, int[] w, int W) {
+        int i = 0;
+        int totalValue = 0;
+        while (W - w[i] > 0) {
+            totalValue += c[i] * w[i];
+            System.out.println("Increment: " + c[i] * w[i]);
+            W -= w[i];
+            System.out.println("w: " + W);
+            // increment i
+            i++;
+        }
+
+        totalValue += c[i] * W;
+
+        return totalValue;
     }
 
-    public void printAge(int age) {
-        System.out.println(age);
-        System.out.println("12");
-    }
 
     public static void main(String[] args) {
-
+        int[] c = {5000, 200, 10};
+        int[] w = {4, 3, 5};
+        System.out.println(maxLoot(c, w, 9));
     }
 }
