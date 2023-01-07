a97255bcaa8a2af96ceb1f75c49e8950d562e86e
Phuc Nguyen <66092633+Saotalangim161099@users.noreply.github.com>
2023-01-07 13:16:28 -0500

    Week 3- Greedy Algorithm

---
+++ src/W3GreedyAlgorithm.java
@@ -0,0 +1,12 @@
+public class W3GreedyAlgorithm {
+    public void printName(String name) {
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+
+    }
+    git clone <https://github.com/PhucGiaNguyen99/AlgorithmicToolbox.git>
+
+    git init
+}
