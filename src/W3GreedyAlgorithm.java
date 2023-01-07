174c61d62b0a9c3493309835222a46dbf83e923a
Phuc Nguyen <66092633+Saotalangim161099@users.noreply.github.com>
2023-01-07 13:35:50 -0500

    Week 3- Greedy Algorithm Testing 2

--- src/W3GreedyAlgorithm.java
+++ src/W3GreedyAlgorithm.java
@@ -5,7 +5,7 @@ public class W3GreedyAlgorithm {
 
     public void printAge(int age) {
         System.out.println(age);
-        System.out.println("11");
+        System.out.println("12");
     }
 
     public static void main(String[] args) {
